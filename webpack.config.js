e55de646e3aaf76f51c9c9bb7bc4ae4d5e881156
Aleks Tsilyurik <ts.alex85@gmail.com>
2017-08-28 14:50:56 +0300

    split webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const devserver = require('./webpack/devserver');
+const pug = require('./webpack/pug');
 
 const PATHS = {
 	src: path.join(__dirname, 'src'),
 	dist: path.join(__dirname, 'dist')
 };
 
-const common = {
-	entry: {
-		'index': PATHS.src + '/pages/index/index.js',
-		'blog': PATHS.src + '/pages/blog/blog.js'
-	},
-	output: {
-		path: PATHS.dist,
-		filename: '[name].js'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			chunks: ['index'],
-			template: PATHS.src + '/pages/index/index.pug'
-		}),
-		new HtmlWebpackPlugin({
-			filename: 'blog.html',
-			chunks: ['blog'],
-			template: PATHS.src + '/pages/blog/blog.pug'
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.pug$/,
-				loader: 'pug-loader',
-				options: {
-					pretty: true
-				}
-			}
+const common = merge([
+	{
+		entry: {
+			'index': PATHS.src + '/pages/index/index.js',
+			'blog': PATHS.src + '/pages/blog/blog.js'
+		},
+		output: {
+			path: PATHS.dist,
+			filename: '[name].js'
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				filename: 'index.html',
+				chunks: ['index'],
+				template: PATHS.src + '/pages/index/index.pug'
+			}),
+			new HtmlWebpackPlugin({
+				filename: 'blog.html',
+				chunks: ['blog'],
+				template: PATHS.src + '/pages/blog/blog.pug'
+			})
 		]
-	}
-};
-
-const developmentConfig = {
-	devServer: {
-		stats: 'errors-only',
-		port: 9000
-	}
-};
+	},
+	pug()
+]);
 
 module.exports = function(env) {
 	if(env === 'production') {
 		return common;
 	}
 	if(env === 'development') {
-		return Object.assign(
-			{},
+		return merge([
 			common,
-			developmentConfig
-		);
+			devserver()
+		]);
 	}
 };
