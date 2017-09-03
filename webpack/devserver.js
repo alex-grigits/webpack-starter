3f3e0344fe7dea4d74b977be46b463241f339aef
alex-grigits <ts.alex85@gmail.com>
2017-09-03 23:53:52 +0300

    include HMR plugin to dev-server config

--- webpack/devserver.js
+++ webpack/devserver.js
@@ -1,10 +1,14 @@
-
+const webpack = require('webpack');
 
 module.exports = function() {
 	return {
 		devServer: {
 			stats: 'errors-only',
-			port: 9000
-		}
+			port: 9000,
+			hot: true
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin()
+		]
 	};
 };
